e6e136c77838c6f4f38d9fa0d900d19b1fe3b0a0
Ktasd Asffa <sapegina212@mail.ru>
2021-03-13 17:06:17 +0300

    Добавьте файлы проекта.

---
+++ WpfApp.CaesarCypher/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp.CaesarCypher
+{
+  public partial class MainWindow : Window
+  {
+    int Key = 0;
+    //const string RussianAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; // 33 symbols
+    //const string EnglishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 26 symbols
+    const string RussianAlphabet = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"; // 66 symbols
+    const string EnglishAlphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"; // 52 symbols
+    List<string> InputText = new List<string>();
+    List<string> OutputText = new List<string>();
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+    // Browse button click event to open file
+    private void buttonBrowse_Click(object sender, RoutedEventArgs e)
+    {
+      OpenFileDialog dialog = new OpenFileDialog()
+      {
+        CheckFileExists = false,
+        CheckPathExists = true,
+        Multiselect = false,
+        Title = "Choose file",
+        Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+      };
+
+      if (dialog.ShowDialog() == true)
+      {
+        tbInput.Text = File.ReadAllText(dialog.FileName, Encoding.UTF8);
+      }
+    }
+    // Encrypt button click event
+    private void buttonEncrypt_Click(object sender, RoutedEventArgs e)
+    {
+      InputText.Clear();
+      OutputText.Clear();
+      Key = Convert.ToInt32(tbKey.Text);
+      for (int i = 0; i < tbInput.LineCount; i++)
+      {
+        InputText.Add(tbInput.GetLineText(i)); // could ToUpper
+      }
+      
+      
+      if(radioEnglish.IsChecked == true)
+      {
+        Encryption(EnglishAlphabet, Key % EnglishAlphabet.Length);
+      }
+      if(radioRussian.IsChecked == true)
+      {
+        Encryption(RussianAlphabet, Key % RussianAlphabet.Length);
+      }  
+    }
+    // Encrypt function
+    private void Encryption(string alphabet, int key)
+    {
+      string newLine = "";
+      foreach (var line in InputText)
+      {
+        for (int i = 0; i < line.Length; i++)
+        {
+          if(alphabet.Contains(line[i].ToString()))
+          {
+            newLine += alphabet[(alphabet.IndexOf(line[i]) + key) % alphabet.Length];
+          }
+          else
+          {
+            newLine += line[i];
+          }
+        }
+        OutputText.Add(newLine);
+        newLine = "";
+      }
+      tbOutput.Clear();
+      for (int i = 0; i < OutputText.Count; i++)
+      {
+        tbOutput.Text += OutputText[i];
+      }
+    }
+    // Decrypt button click event
+    private void buttonDecrypt_Click(object sender, RoutedEventArgs e)
+    {
+      InputText.Clear();
+      OutputText.Clear();
+      Key = Convert.ToInt32(tbKey.Text);
+      for (int i = 0; i < tbInput.LineCount; i++)
+      {
+        InputText.Add(tbInput.GetLineText(i));
+      }
+
+
+      if (radioEnglish.IsChecked == true)
+      {
+        Decryption(EnglishAlphabet, Key % EnglishAlphabet.Length);
+      }
+      if (radioRussian.IsChecked == true)
+      {
+        Decryption(RussianAlphabet, Key % RussianAlphabet.Length);
+      }
+    }
+    // Decrypt function
+    private void Decryption(string alphabet, int key)
+    {
+      string newLine = "";
+      foreach (var line in InputText)
+      {
+        for (int i = 0; i < line.Length; i++)
+        {
+          if (alphabet.Contains(line[i].ToString()))
+          {
+            newLine += alphabet[(alphabet.IndexOf(line[i]) + alphabet.Length - key) % alphabet.Length];
+          }
+          else
+          {
+            newLine += line[i];
+          }
+        }
+        OutputText.Add(newLine);
+        newLine = "";
+      }
+      tbOutput.Clear();
+      for (int i = 0; i < OutputText.Count; i++)
+      {
+        tbOutput.Text += OutputText[i];
+      }
+    }
+    // Browse button click event to save file
+    private void buttonSave_Click(object sender, RoutedEventArgs e)
+    {
+      SaveFileDialog dialog = new SaveFileDialog()
+      {
+        Title = "Save file",
+        Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+      };
+      if (dialog.ShowDialog() == true)
+      {
+        File.WriteAllText(dialog.FileName, tbOutput.Text, Encoding.UTF8);
+      }
+    }
+    // Only numeric input in key textbox
+    private void tbKey_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+      if (!Char.IsDigit(e.Text, 0))
+      {
+        e.Handled = true;
+      }
+    }
+  }
+}
